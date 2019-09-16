4c46442e0c0027ae9057ee557a4fa7c9980c3c2b
root <root@ip-172-31-72-181.ec2.internal>
2019-09-16 13:30:44 +0000

    Changed env variable text

--- ecoScripts/service_now/Image/run_integration.py
+++ ecoScripts/service_now/Image/run_integration.py
@@ -7,9 +7,11 @@ pigeon = Pigeon()
 
 def main():
     pigeon.sendInfoMessage("Starting integration")
+    pipeline = subprocess.Popen('java -cp data-pipeline-bundled-0.1.jar org.cisco.aurora.pipeline.AuroraDataPipeline --runner=FlinkRunner --flinkMaster=localhost --filesToStage=target/data-pipeline-bun-0.1.jar --kafkaIP=$KAFKA_IP --kafkaPort=$KAFKA_PORT --kafkaInputTopic=$KAFKA_INPUT_TOPIC --kafkaOutputTopic=$KAFKA_OUTPUT_TOPIC --streaming=true --parallelism=1')
     consumer = subprocess.Popen(["python", "-m", "snow.consumer.app"])
     connector = subprocess.Popen(["python", "-m", "snow.snow-table-parser.aurora_snow_connector"])
 
+    pipeline.wait()
     consumer.wait()
     connector.wait()
 
