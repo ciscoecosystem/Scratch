a986bc685d2e06648d7ca7b87c40328469b83c2d
Dipika Mansukhani <dipika.mansukhani@crestdatasys.com>
2019-10-01 12:38:44 +0530

    Changes for taking job id

--- ecoScripts/service_now/Image/run_integration.py
+++ ecoScripts/service_now/Image/run_integration.py
@@ -1,9 +1,10 @@
 import subprocess
 import os
-
+from shlex import split
 from pigeon import Pigeon
 
 pigeon = Pigeon()
+
 
 def main():
     pigeon.sendInfoMessage("Starting integration")
@@ -14,7 +15,23 @@ def main():
     kafka_input_topic = os.getenv('KAFKA_INPUT_TOPIC')
     kafka_output_topic = os.getenv('KAFKA_OUTPUT_TOPIC')
 
-    pipeline = subprocess.Popen(["java", "-jar", "/app/data-pipeline-bundled-0.1.jar", "--runner=FlinkRunner", "--flinkMaster={}".format(flink_ip), "--kafkaIP={}".format(kafka_ip), "--kafkaPort={}".format(kafka_port), "--kafkaInputTopic={}".format(kafka_input_topic), "--kafkaOutputTopic={}".format(kafka_output_topic), "--streaming=true", "--parallelism=1"])
+    pipeline = subprocess.Popen(split(["java", "-jar", "/app/data-pipeline-bundled-0.1.jar", "--runner=FlinkRunner",
+                                       "--flinkMaster={}".format(flink_ip), "--kafkaIP={}".format(kafka_ip),
+                                       "--kafkaPort={}".format(kafka_port),
+                                       "--kafkaInputTopic={}".format(kafka_input_topic),
+                                       "--kafkaOutputTopic={}".format(kafka_output_topic), "--streaming=true",
+                                       "--parallelism=1"]), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    str_job = "Submitting job"
+    while True:
+        line = pipeline.stdout.readline()
+        if not line:
+            break
+        if str_job in line:
+            print("Got line having job id from the pipeline process from run_integration.py :", line.rstrip())
+            job_id_str = line.rstrip().split(str_job)[1].lstrip().split(' ')
+            job_id = job_id_str[0]
+            print("Got job id from run_integration.py ", job_id)
+
     consumer = subprocess.Popen(["python", "-m", "snow.consumer.app"])
     connector = subprocess.Popen(["python", "-m", "snow.snow-table-parser.aurora_snow_connector"])
 
@@ -22,6 +39,6 @@ def main():
     consumer.wait()
     connector.wait()
 
+
 if __name__ == '__main__':
     main()
-
